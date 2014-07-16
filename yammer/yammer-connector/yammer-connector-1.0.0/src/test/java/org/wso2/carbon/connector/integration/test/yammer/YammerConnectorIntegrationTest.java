87f5c019c5f29a54e848eb9699942800790d3287
Manil Liyanage <liyanagemanil@gmail.com>
2014-07-16 11:58:34 +0530

    sd

--- yammer/yammer-connector/yammer-connector-1.0.0/src/test/java/org/wso2/carbon/connector/integration/test/yammer/YammerConnectorIntegrationTest.java
+++ yammer/yammer-connector/yammer-connector-1.0.0/src/test/java/org/wso2/carbon/connector/integration/test/yammer/YammerConnectorIntegrationTest.java
@@ -18,68 +18,67 @@
 
 package org.wso2.carbon.connector.integration.test.yammer;
 
-import org.apache.axis2.context.ConfigurationContext;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.wso2.carbon.automation.api.clients.proxy.admin.ProxyServiceAdminClient;
-import org.wso2.carbon.automation.api.clients.utils.AuthenticateStub;
-import org.wso2.carbon.automation.utils.axis2client.ConfigurationContextProvider;
-
-import org.wso2.carbon.esb.ESBIntegrationTest;
-import org.wso2.carbon.mediation.library.stub.MediationLibraryAdminServiceStub;
-import org.wso2.carbon.mediation.library.stub.upload.MediationLibraryUploaderStub;
 import org.wso2.connector.integration.test.base.ConnectorIntegrationTestBase;
 import org.wso2.connector.integration.test.base.RestResponse;
 
-
-import javax.activation.DataHandler;
-import java.io.IOException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
+
 
 public class YammerConnectorIntegrationTest extends ConnectorIntegrationTestBase {
 
+    protected static final String CONNECTOR_NAME = "yammer-connector-1.0.0";
+    private String validAuthorization;
+    private String invalidAuthorization;
 
 
-    @BeforeClass(alwaysRun = true)
-    public void setEnvironment() throws Exception {
-        init("yammer-connector-1.0.0");
+    private Map<String, String> esbRequestHeadersMap = new HashMap<String, String>();
+
+    private Map<String, String> apiRequestHeadersMap = new HashMap<String, String>();
+
+    private Map<String, String> parametersMap = new HashMap<String, String>();
+
+    @BeforeTest(alwaysRun = true)
+    protected void init() throws Exception {
+        super.init(CONNECTOR_NAME);
+
+        esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
+        esbRequestHeadersMap.put("Content-Type", "application/json");
+
+        apiRequestHeadersMap.put("Authorization","Bearer " + connectorProperties.getProperty("token"));
+        apiRequestHeadersMap.put("Accept-Charset", "UTF-8");
+        apiRequestHeadersMap.put("Content-Type", "application/json");
+    }
+
+
+    @Override
+    protected void cleanup() {
+        axis2Client.destroy();
     }
 
     private String addCredentials(String jsonString) {
         return String.format(
                 jsonString,
-                connectorProperties.getProperty("yammer_token"),
+                connectorProperties.getProperty("token"),
                 connectorProperties.getProperty("Apiurl"));
     }
 
 
-    /**
-     * Oauth Positive test case for postGetAll method with mandatory parameters.
-     */
+
     @Test(priority = 1, groups = {"wso2.esb"}, description = "yammer {getFollowing} integration test with mandatory parameters")
-    public void testYammerGetFollowingWithMandatoryParameters() throws Exception {
+    public void testYammerGetFollowingWithMandatory() throws Exception {
 
-        String jsonRequestFilePath = pathToResourcesDirectory + "GetFollowing.txt";
+        esbRequestHeadersMap.put("Action", "urn:getUser");
+        String apiEndPoint =  connectorProperties.getProperty("Apiurl") + "/v1/messages/following.json";
 
-        String rawString = getFileContent(jsonRequestFilePath);
-        rawString = rawString.replace("clientId",connectorProperties.getProperty("client_id"));
-        rawString = rawString.replace("clientSecret",connectorProperties.getProperty("client_secret"));
-        final String jsonString = addCredentials(rawString);
+        RestResponse<JSONObject> esbRestResponse = sendJsonRestRequest(proxyUrl, "POST", esbRequestHeadersMap,"GetFollowing.json");
+        RestResponse<JSONObject> apiRestResponse = sendJsonRestRequest(apiEndPoint, "GET", apiRequestHeadersMap);
 
-        ConnectorIntegrationUtil responseConnector = new ConnectorIntegrationUtil();
-        OMElement omElementC = responseConnector.getXmlResponse("POST", getProxyServiceURL("delicious"), jsonString);
-
-        ConnectorIntegrationUtil responseDirect = new ConnectorIntegrationUtil();
-        OMElement omElementD = responseDirect.sendXMLRequestWithBasic(connectorProperties.getProperty("Apiurl") + "/v1/posts/all", "", validAuthorization);
-
-        Assert.assertTrue(omElementC.getFirstElement().toString().equals(omElementD.getFirstElement().toString()));
+        Assert.assertEquals(esbRestResponse.getBody().get("threaded_extended").toString(), apiRestResponse.getBody().get("threaded_extended").toString());
     }
 
 
